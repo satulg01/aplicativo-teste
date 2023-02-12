b160275e5092cb7e98d4d6ae6468b7dc72e48a52
Roger <roger.r.oliveira@unesp.br>
2023-02-12 09:41:03 -0300

    Ajustes base

--- application/views/components/html/header.php
+++ application/views/components/html/header.php
@@ -9,12 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Satulg <?php echo (isset($title) ? "- " . $title : '') ?></title>
     
-    <link href="<?= base_url('assets/css/') ?>bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="<?= base_url('assets/css/') ?>bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
     <link href="<?= base_url('assets/css/') ?>Core.css" rel="stylesheet" crossorigin="anonymous">
     <link href="<?= base_url('assets/css/') ?>Status.css" rel="stylesheet" crossorigin="anonymous">
     <link href="<?= base_url('assets/') ?>fonts.css" rel="stylesheet" crossorigin="anonymous">
 
-    <script src="<?= base_url('assets/js/') ?>bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/js/') ?>bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url('assets/js/') ?>jquery-3.6.1.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url('assets/js/') ?>popper.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url('assets/js/') ?>jquery.mask.js" crossorigin="anonymous"></script>
