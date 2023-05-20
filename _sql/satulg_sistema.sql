c2699fd807549527458b0104759a82126112a336
Roger <roger.r.oliveira@unesp.br>
2023-05-19 21:26:21 -0300

    sql banco

---
+++ _sql/satulg_sistema.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13-Fev-2023 às 03:41
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `aplicativo_teste`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `blocked_ips`
+--
+
+CREATE TABLE `blocked_ips` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(30) NOT NULL,
+  `block_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `block_expires` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `collaborators`
+--
+
+CREATE TABLE `collaborators` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `document` varchar(14) NOT NULL,
+  `zip_code` varchar(10) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(80) NOT NULL,
+  `access` varchar(2) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login_attemps`
+--
+
+CREATE TABLE `login_attemps` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `user` varchar(200) NOT NULL,
+  `pass` varchar(200) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `date_sale` datetime NOT NULL DEFAULT current_timestamp(),
+  `observation` text NOT NULL,
+  `value` decimal(10,2) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `orders_items`
+--
+
+CREATE TABLE `orders_items` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `quantity` decimal(10,2) NOT NULL,
+  `value` decimal(10,2) NOT NULL,
+  `value_final` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `description` text NOT NULL,
+  `brand` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `id_collaborator` int(11) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `document` varchar(14) NOT NULL,
+  `pass` varchar(200) NOT NULL,
+  `access` varchar(4) NOT NULL,
+  `token` varchar(200) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `id_collaborator`, `user`, `document`, `pass`, `access`, `token`, `status`) VALUES
+(1, 'Teste', 0, 'admin', '999.999.999-99', '2534f8a88242807abc33fbb6fa0ca96dea562b0a', 'A', '', 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `blocked_ips`
+--
+ALTER TABLE `blocked_ips`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `collaborators`
+--
+ALTER TABLE `collaborators`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `login_attemps`
+--
+ALTER TABLE `login_attemps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `orders_items`
+--
+ALTER TABLE `orders_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `blocked_ips`
+--
+ALTER TABLE `blocked_ips`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `collaborators`
+--
+ALTER TABLE `collaborators`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `login_attemps`
+--
+ALTER TABLE `login_attemps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `orders_items`
+--
+ALTER TABLE `orders_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
